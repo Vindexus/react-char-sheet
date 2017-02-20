04b864d8cda13fa9398b2b4bf4e3786361f1a986
Vindexus <vindexus@gmail.com>
2017-02-19 17:12:01 -0800

    Multiple characters

--- src/js/actions/character.js
+++ src/js/actions/character.js
@@ -16,13 +16,13 @@ export const downgradeStat = (character, key) => {
   return modifyStat(character, key, -1)
 }
 
-export const fetchCharacter = () => {
+export const fetchCharacter = (id) => {
   return (dispatch) => {
     dispatch({
       type: 'FETCH_CHARACTER_PENDING'
     })
 
-    axios.get("http://rest.learncode.academy/api/vindexus/characters/58a93858c0be46010096439b")
+    axios.get("http://rest.learncode.academy/api/vindexus/characters/" + id)
         .then((resp) => {
             dispatch({
               type: 'FETCH_CHARACTER_FULFILLED',
@@ -38,6 +38,59 @@ export const fetchCharacter = () => {
   }
 }
 
+export const fetchCharacters = (id) => {
+  return (dispatch) => {
+    dispatch({
+      type: 'FETCH_CHARACTERS_PENDING'
+    })
+
+    axios.get("http://rest.learncode.academy/api/vindexus/characters")
+        .then((resp) => {
+            dispatch({
+              type: 'FETCH_CHARACTERS_FULFILLED',
+              payload: resp.data
+            })
+        })
+        .catch((err) => {
+          dispatch({
+            type: 'FETCH_CHARACTERS_REJECTED',
+            payload: err
+          })
+        })
+  }
+}
+
+export const addCharacter = () => {
+  return (dispatch => {
+    const defaultCharacter = {
+      name: 'Vindexus ' + new Date().getTime().toString().substr(-1, 3),
+      stats: {
+        strength: 0,
+        wits: 0,
+        aim: 0,
+        speed: 0
+      }
+    }
+    dispatch({
+      type: 'ADD_CHARACTER_PENDING'
+    })
+    axios.post('http://rest.learncode.academy/api/vindexus/characters', defaultCharacter)
+      .then((resp) => {
+        dispatch({
+          type: 'ADD_CHARACTER_FULFILLED',
+          payload: resp.data
+        })
+        dispatch(fetchCharacters())
+      })
+      .catch((err) => {
+        dispatch({
+          type: 'ADD_CHARACTER_REJECTED',
+          payload: err
+        })
+      })
+  })
+}
+
 export const modifyStat = (character, key, amount) => {
   return (dispatch) => {
     dispatch({
@@ -50,13 +103,13 @@ export const modifyStat = (character, key, amount) => {
     stat = Math.max(stat, -1)
     character.stats[key] = stat
 
-    axios.put('http://rest.learncode.academy/api/vindexus/characters/58a93858c0be46010096439b', character)
+    axios.put('http://rest.learncode.academy/api/vindexus/characters/' + character.id, character)
         .then((resp) => {
           dispatch({
             type: 'MODIFY_STAT_FULFILLED',
-            payload: resp
+            payload: resp.data
           })
-          dispatch(fetchCharacter())
+          dispatch(fetchCharacter(character.id))
         })
         .catch((err) => {
           dispatch({
